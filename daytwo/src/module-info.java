760b483217115a5b2343a61b00a5e597145a6a99
Admin <Admin@Admin-PC>
2023-07-06 13:05:21 +0530

    first commit

---
+++ daytwo/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Admin
+ *
+ */
+module daytwo {
+	requires java.persistence;
+}
---
+++ daytwo/src/org/sjcem/joinedtableinheritence/Car.java
@@ -0,0 +1,23 @@
+package org.sjcem.joinedtableinheritence;
+import javax.persistence.Entity;
+
+import javax.persistence.Table;
+
+//child class
+@Entity
+@Table(name="Car")
+public class Car extends Vehicle {
+private String brandName;
+
+	
+	
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	
+
+}
